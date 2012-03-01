db505ecfc48fcf82aac7fdb464676d465d8db6fe
Yusuke Nomura <yunomu@gmail.com>
2012-03-01 04:49:52 +0000

    ガントチャートのソートに対応

--- init.rb
+++ init.rb
@@ -5,14 +5,24 @@ require File.dirname(__FILE__) + '/lib/redmine_projects_sorter'
 
 Dispatcher.to_prepare :redmine_projects_sorter do
   require_dependency 'projects_helper'
-  ProjectsHelper.send(:include, Redmine::Plugins::ProjectsSorter) 
+  ProjectsHelper.send(:include, Redmine::Plugins::ProjectsSorter::ProjectsHelperPatch)
+end
+
+class << Project
+  include Redmine::Plugins::ProjectsSorter::ProjectPatch::ClassMethods
+
+  def project_tree(projects, &block)
+    flatten(sortTree(construct(projects), :name)).each do |p, l|
+      yield p, l
+    end
+  end
 end
 
 Redmine::Plugin.register :redmine_projects_sorter do
   name 'Redmine Projects Sort plugin'
   author 'Yusuke Nomura'
   description 'This is a plugin for sorting projects of Redmine'
-  version '0.0.1'
+  version '0.0.2'
   url 'http://yunomu.hatenablog.jp/'
   author_url 'https://github.com/yunomu'
 end
